7efccbe5742cd4dc3e8fb57ea9f43262650ca483
ZarbaliyevOzal <34451434+ZarbaliyevOzal@users.noreply.github.com>
2022-02-16 12:36:15 +0400

    fixed default mail layout

--- server/src/app/mail/VerificationEmail.js
+++ server/src/app/mail/VerificationEmail.js
@@ -12,10 +12,16 @@ class VerificationEmail {
     this.user = user
   }
 
+  layout(content) {
+    const str = fs.readFileSync(path.join(__dirname, '../../views/vendor/mail/default.ejs'), 'utf-8')
+    return ejs.render(str, { content })
+  }
+
   build() {
     const url = `${process.env.APP_URL}/verify-email/${this.user.id}?token=${this.token}`
     const str = fs.readFileSync(path.join(__dirname, '../../views/email/verification.ejs'), 'utf-8')
-    return ejs.render(str, { url })
+    const content = ejs.render(str, { url })
+    return this.layout(content)
   }
 
   async queue(to) {
