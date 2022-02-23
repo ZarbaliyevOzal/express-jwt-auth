74883abaf5a8aa73161ae7f62ab1a59a8c3880fb
ZarbaliyevOzal <34451434+ZarbaliyevOzal@users.noreply.github.com>
2022-02-23 12:35:13 +0400

    fixed links

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -230,12 +230,16 @@ const recentPosts = [
                 {{ item.name }}
               </a>
             </div>
-            <div>
-              <a href="#" class="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"> Sign up </a>
+            <div v-if="!isAuthenticated">
+              <router-link :to="{ name: 'SignUp' }" class="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"> 
+                Sign up 
+              </router-link>
               <p class="mt-6 text-center text-base font-medium text-gray-500">
                 Existing customer?
                 {{ ' ' }}
-                <a href="#" class="text-indigo-600 hover:text-indigo-500"> Sign in </a>
+                <router-link :to="{ name: 'SignIn' }" class="text-indigo-600 hover:text-indigo-500"> 
+                  Sign in 
+                </router-link>
               </p>
             </div>
           </div>
